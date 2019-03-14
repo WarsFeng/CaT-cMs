77263e822f6b59ef3cc8178dba92a61a3bf40429
WarsFeng <huangrongfeng1999@gmail.com>
2019-03-14 19:34:11 +0800

    feat(all): Cms manager created

---
+++ cms-service-manager/src/main/java/cat/wars/cms/CmsManagerApplication.java
@@ -0,0 +1,28 @@
+package cat.wars.cms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: wars
+ * Date: 3/14/19
+ * Time: 2:20 PM
+ * Cms manager main
+ */
+
+@SpringBootApplication
+@EntityScan("cat.wars.cms.framework.domain.cms")
+@ComponentScans({
+        @ComponentScan(basePackages = {"cat.wars.cms.manager"}),
+        @ComponentScan(basePackages = {"cat.wars.cms.api.cms"})
+})
+public class CmsManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CmsManagerApplication.class, args);
+    }
+}
