a55dd6d4b20551449f09629bdb776f1685915b0d
WarsFeng <huangrongfeng1999@gmail.com>
2019-05-29 22:24:26 +0800

    feat(all): Inited manager file system module

---
+++ cms-service-fs/src/main/java/cat/wars/cms/manager_fs/CmsManagerFileSystemApplication.java
@@ -0,0 +1,19 @@
+package cat.wars.cms.manager_fs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: wars
+ * Date: 5/29/19
+ * Time: 9:52 PM
+ */
+
+@SpringBootApplication
+public class CmsManagerFileSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CmsManagerFileSystemApplication.class, args);
+    }
+}
